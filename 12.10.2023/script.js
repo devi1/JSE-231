6665180e39cb8ca67eed1a4bc60853fdc78ac9af
Damir <damiryessenbek@gmail.com>
2023-10-12 20:43:44 +0500

    Cookies an HW

---
+++ 12.10.2023/script.js
@@ -0,0 +1,6 @@
+// document.cookie = "name=Damir; path=/admin"; //ключ=значение в данном случае у нас это name=Damir, path позволяет задать параметр работы на определенной странице
+// console.log(document.cookie);
+
+document.cookie = "name=Damir; path=/admin; secure=true";
+console.log(document.cookie);
+
