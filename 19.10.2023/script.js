d6eef3bf40039c39ef777cbe1dad1a37f08a4d60
Damir <damiryessenbek@gmail.com>
2023-10-19 20:13:07 +0600

    JSON

--- 19.10.2023/script.js
+++ 19.10.2023/script.js
@@ -1,21 +1,46 @@
-let user = {
-    name: 'Ivan',
-    age: 25,
-    city: 'Sofia'
+// let user = {
+//     name: 'Ivan',
+//     age: 25,
+//     city: 'Sofia',
+//     bool: true,
+//     arr1: [1, 2, 3, 4, 5],
+//     arr2: ["asdf", "asdf", "asdf", "asdf"]
+// }
+
+// console.log(JSON.stringify(user)); // Объект -> JSON
+// console.log(JSON.parse(JSON.stringify(user))); // JSON -> Объект
+
+// let user2 = '{"name""Ivan","age":25,"city":"Sofia", "bool":true,"arr1":[1,2,3,4,5],"arr2":["asd","asdf","asdf","asdf"]}'
+// console.log(user2);
+// let objectUser2 = JSON.parse(user2);
+
+// let name = objectUser2.name;
+// let age = objectUser2.age;
+// let city = objectUser2.city;
+
+// console.log(name);
+// console.log(age);
+// console.log(city);
+
+let student = {
+    sayHI(){
+        alert("HI");
+    },
+    smthng: undefined,
+    outofcore: null,
+    user: {
+        name: 'Ivan',
+        sayBye(){
+            alert("Bye");
+        },
+        wtf:undefined,
+        age: 25,
+        city: 'Sofia',
+        bool: true,
+        arr1: [1, 2, 3, 4, 5]
+    }
 }
 
-console.log(JSON.stringify(user)); // Объект -> JSON
-console.log(JSON.parse(JSON.stringify(user))); // JSON -> Объект
-
-let user2 = '{"name":"Ivan","age":25,"city":"Sofia"}'
-console.log(user2);
-let objectUser2 = JSON.parse(user2);
-
-let name = objectUser2.name;
-let age = objectUser2.age;
-let city = objectUser2.city;
-
-
-console.log(name);
-console.log(age);
-console.log(city);
+console.log(student);
+console.log(JSON.stringify(student));
+console.log(JSON.parse(JSON.stringify(student)));
