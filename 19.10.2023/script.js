fc46e20c95835ffdcf726ee9ec53a74685bf71d5
Damir <damiryessenbek@gmail.com>
2023-10-19 20:51:14 +0600

    JSON, AJAX

--- 19.10.2023/script.js
+++ 19.10.2023/script.js
@@ -22,25 +22,57 @@
 // console.log(age);
 // console.log(city);
 
-let student = {
-    sayHI(){
-        alert("HI");
-    },
-    smthng: undefined,
-    outofcore: null,
-    user: {
-        name: 'Ivan',
-        sayBye(){
-            alert("Bye");
-        },
-        wtf:undefined,
-        age: 25,
-        city: 'Sofia',
-        bool: true,
-        arr1: [1, 2, 3, 4, 5]
+// let student = {
+//     sayHI(){
+//         alert("HI");
+//     },
+//     smthng: undefined,
+//     outofcore: null,
+//     user: {
+//         name: 'Ivan',
+//         sayBye(){
+//             alert("Bye");
+//         },
+//         wtf:undefined,
+//         age: 25,
+//         city: 'Sofia',
+//         bool: true,
+//         arr1: [1, 2, 3, 4, 5]
+//     }
+// }
+
+// console.log(student);
+// console.log(JSON.stringify(student));
+// console.log(JSON.parse(JSON.stringify(student)));
+document.getElementById("load").addEventListener("click", function () {
+    let xhr = new XMLHttpRequest();
+    
+    // 100-199 Информационные статусы
+    // 200-299 Успешные статусы
+    // 300-399 Статусы перенаправления
+    // 400-499 Ошибки со стороны пользователей
+    // 500-599 Ошибки со стороны сервера
+    
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            let data = JSON.parse(xhr.responseText);
+            console.log(data);
+            displayInfo(data);
+        }
+    }
+    
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+    xhr.send();
+})
+
+function displayInfo(data){
+    let container = document.getElementById("container");
+    container.innerHTML = "";
+    for (let i = 0; i < data.length; i++) {
+        let item = data[i];
+        let span = document.createElement("li");
+        span.textContent = item.name;
+
+        container.append(span);
     }
 }
-
-console.log(student);
-console.log(JSON.stringify(student));
-console.log(JSON.parse(JSON.stringify(student)));
