65b57b0c26edf6b8a207e4c90ec36d5f14bb14b5
Damir <damiryessenbek@gmail.com>
2023-10-26 21:10:53 +0600

    fetch

--- 26.10.2023/script.js
+++ 26.10.2023/script.js
@@ -1,6 +1,7 @@
 // Получение данных
 
-// let url = "https://jsonplaceholder.typicode.com/posts;";
+// let url = "https://jsonplaceholder.typicode.com/users;";
+// console.log(fetch(url));
 
 // fetch(url, {
 //     method: "GET",
@@ -25,24 +26,50 @@
 
 // Отпарвка данных
 
-let fileInput = document.getElementById("picture");
-let form = document.getElementById("form");
+// let fileInput = document.getElementById("picture");
+// let form = document.getElementById("form");
 
-function handleSubmit(event){
-    event.preventDefault();
+// function handleSubmit(event){
+//     event.preventDefault();
     
-    let formData = new FormData(form);
+//     let formData = new FormData(form);
 
-    for(let i = 0; i < fileInput.files.length; i++){
-        let file = fileInput.files[i];
-        formData.append("picture", file, file.name);
+//     for(let i = 0; i < fileInput.files.length; i++){
+//         let file = fileInput.files[i];
+//         formData.append("picture", file, file.name);
+//     }
+
+//     const url = "https://console.firebase.google.com/u/0/project/back-50cca/database/back-50cca-default-rtdb/data/~2F"
+//     fetch(url, {
+//         method: "POST",
+//         body: formData
+//     })
+// }
+
+// form.addEventListener("submit", handleSubmit);
+
+
+// Загрузка данных
+
+let url = "https://i.imgur.com/C5QXZ7u.mp4";
+
+fetch(url).then(async (response) => {
+    let cnt = 0;
+
+    const reader = response.body.getReader();
+
+    const contentLength = parseInt(response.headers.get("content-length"), 10);
+
+    while (true) {
+        const { done, value } = await reader.read();
+        if (done) {
+            console.log("Done 100%");
+            break;
+        }
+
+        cnt += Math.ceil(contentLength / value.length);
+
+        console.log(`Recieved ${cnt}%`);
     }
 
-    const url = "backend"
-    fetch(url, {
-        method: "POST",
-        body: formData
-    })
-}
-
-form.addEventListener("submit", handleSubmit);
+})
